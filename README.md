07e8b324317385d7946d49b524bd31e87e9fa11b
Anna <renita5701@gmail.com>
2022-04-04 08:27:22 +0600

    created feign for employee-core-api service

---
+++ README.md
@@ -0,0 +1,5 @@
+# company-api
+
+This is an example of *company management service* to demonstrate how Spring Cloud OpenFeign works.
+
+**DAR Academy's Winter Internship 2022**
---
+++ src/main/java/kz/dar/academy/backend/feign/EmployeeFeign.java
@@ -0,0 +1,10 @@
+package kz.dar.academy.backend.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient("employee-core-api")
+public interface EmployeeFeign {
+    @GetMapping("/employee/check")
+    String checkEmployee();
+}
